69a6c1eaf8f863e6f4aaa3bae2228fe00f78d202
Richard Moss <richard.moss@cyotek.com>
2022-07-14 21:39:23 +0100

    NEW: Implemented SaveChanges

--- src/Exif.cs
+++ src/Exif.cs
@@ -201,7 +201,17 @@ namespace Cyotek.FixExif
 
           foreach (Tuple<CommandType, string> command in pair.Value)
           {
-            Console.WriteLine("\t{0}: {1}", command.Item1, command.Item2);
+            switch (command.Item1)
+            {
+              case CommandType.ExifTool:
+                Console.WriteLine(this.Run(string.Format("{0} \"{1}\"", command.Item2, pair.Key)));
+                break;
+              case CommandType.SetLastWriteTimeUtc:
+                File.SetLastWriteTimeUtc(pair.Key, DateTime.FromBinary(long.Parse(command.Item2)));
+                break;
+              default:
+                throw new ArgumentOutOfRangeException();
+            }
           }
         }
 
